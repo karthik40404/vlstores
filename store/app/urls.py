c00efbf2a99e2d90a2e831662206e2c57105a327
karthik40404 <karpython123@gmail.com>
2024-12-10 00:14:03 +0530

    not_finished_baaad

--- store/app/urls.py
+++ store/app/urls.py
@@ -4,6 +4,9 @@ from .import views
 urlpatterns = [
     path('',views.log),
     path('register',views.reg),
-    path('lout',views.logout),
+    path('lout',views.lout),
     path('admdash',views.admdash),
+    path('add/',views.addp),
+    path('edit/',views.editp),
+    path('dele/',views.delp),
 ]
--- store/app/views.py
+++ store/app/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def log(req):
-    if req.method=="POST":
-        email=req.POST['email']
-        psw=req.POST['psw']
-        data=authenticate(email=email,password=psw)
-        data.save()
+    if 'shop' in req.session:
         return redirect(admdash)
-    else:
-        return render(req,'login.html')
+    if req.method=="POST":
+        email = req.POST['email']
+        psw = req.POST['psw']
+        print(email,psw)
+        data = authenticate(username=email, password=psw)
+        if data:
+            login(req, data)
+            if data.is_superuser:
+                req.session['shop'] = email
+                return redirect(admdash)
+        else:
+            messages.warning(req, "Incorrect username or password.")
+            return redirect(log) 
+    return render(req,'login.html')
 
 def reg(req):
     if req.method=="POST":
@@ -25,9 +34,18 @@ def reg(req):
         return render(req,'registraion.html')
 
 def admdash(req):
-    return redirect(req,'dash.html')
+    return render(req,'shop/dash.html')
 
-def logout(req):
+def lout(req):
      logout(req)
      req.session.flush()
      return redirect(log)
+
+def addp(req):
+    return render(req,'shop/addproduct.html')
+
+def editp(req):
+    return render(req,'shop/editproduct.html')
+
+def delp(req):
+    return render(req,'shop/deleteproduct.html')
